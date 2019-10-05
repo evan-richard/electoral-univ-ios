b665711f5c4dc70894cc893c420f1057f79f4133
Evan Richard <evan.richard.umd@gmail.com>
2019-10-04 21:03:08 -0400

    Initial Commit

---
+++ ElectoralUniversity/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ElectoralUniversity
+//
+//  Created by Evan Richard on 10/4/19.
+//  Copyright © 2019 EvanRichard. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
